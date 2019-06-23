4e1569fb1f9e0648e8304f63c3f4f6635df8ef85
Andrey Grach <andrejgrach@icloud.com>
2019-06-24 01:17:35 +0300

    addition to last commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_205254) do
+ActiveRecord::Schema.define(version: 2019_06_23_213244) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
