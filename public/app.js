59835245fb7021e866079229a3f3c0928e36a972
Andrey Grach <andrejgrach@icloud.com>
2019-06-27 01:09:42 +0300

    Added div

--- public/app.js
+++ public/app.js
@@ -1,6 +1,12 @@
+function add_to_cart(id)
+{
+   alert('Hello from function!');
+}
+
 function something()
 { var x = window.localStorage.getItem('ccc');
 x = x * 1 + 1;
 window.localStorage.setItem('ccc', x);
 alert (x);
 }
+
