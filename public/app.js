c5b6a67d5c4bde3ba8e9f7813a1be584f229fb3e
Andrey Grach <andrejgrach@icloud.com>
2019-06-28 21:42:47 +0300

    added function cart_get_number_of_items

--- public/app.js
+++ public/app.js
@@ -1,11 +1,3 @@
-function add_to_cart(id)
-{
-    var key = 'product_' + id;
-   var x = window.localStorage.getItem(key);
-    x = x * 1 + 1;
-    window.localStorage.setItem(key, x);
-}
-
 function something()
 { var x = window.localStorage.getItem('ccc');
 x = x * 1 + 1;
@@ -13,3 +5,27 @@ window.localStorage.setItem('ccc', x);
 alert (x);
 }
 
+function add_to_cart(id)
+{
+    var key = 'product_' + id;
+    var x = window.localStorage.getItem(key);
+    x = x * 1 + 1;
+    window.localStorage.setItem(key, x);
+ // вывод кол-ва itemщв в корзине
+    alert('Items in your cart: ' + cart_get_number_of_items());
+}
+
+function cart_get_number_of_items()
+{   var cnt = 0;
+    for (var i = 0; i < window.localStorage.length; i++)
+{
+    var key = window.localStorage.key(i); //получаем ключ
+    var value = window.localStorage.getItem(key); //получаем значение
+
+    if(key.indexOf('product_') == 0)
+    {
+        cnt = cnt + value * 1;
+    }
+}
+    return cnt;
+}
