66dac4ed093aaafe5f370dffe908c7dca8962522
Andrey Grach <andrejgrach@icloud.com>
2019-07-01 01:53:17 +0300

    added cart_get_orders function at js and relocated cart.erb

--- public/app.js
+++ public/app.js
@@ -29,3 +29,17 @@ function cart_get_number_of_items()
 }
     return cnt;
 }
+
+function cart_get_orders()
+{   var orders = '';
+    for (var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i); //получаем ключ
+        var value = window.localStorage.getItem(key); //получаем значение
+
+        if(key.indexOf('product_') == 0) {
+            orders = orders + key + '=' + value + ',';
+        }
+    }
+    return orders;
+}
