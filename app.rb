7f7ac6454e158316150a08521143ef6f7ca286e7
Andrey Grach <andrejgrach@icloud.com>
2019-07-12 22:48:55 +0300

    Added place order inputs

--- app.rb
+++ app.rb
@@ -33,14 +33,13 @@ end
 
 post '/cart' do
 
-  orders_input = params[:orders]
+  @orders_input = params[:orders]
 
-  @items = parse_orders_input orders_input
+  @items = parse_orders_input @orders_input
 
   @items.each do |item|
     item[0] = Product.find(item[0])
   end
-
 
   erb :cart
 end
